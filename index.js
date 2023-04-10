28f43f9a773e30109f5c7b1b8dedb7441bd986cd
Ruth <rwangare124@gmail.com>
2023-04-10 11:26:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
-/* 
+function isPalindrome(word) {
+  // Write your algorithm here
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // compare the reversed string to the input
+    return word === reversedWord;
+}
+
+/*
   Add your pseudocode here
 */
 
@@ -16,7 +24,11 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("fantasy"));
+
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("dad"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
--- test/index.js
+++ test/index.js
@@ -9,11 +9,17 @@ describe("isPalindrome", () => {
   it("returns true for 'racecar'", () => {
     expect(isPalindrome("racecar")).to.be.true;
   });
+  it("returns true for 'dad'", () => {
+    expect(isPalindrome("dad")).to.be.true;
+  });
   it("returns true for 'a'", () => {
     expect(isPalindrome("a")).to.be.true;
   });
   it("returns false for 'robot'", () => {
     expect(isPalindrome("robot")).to.be.false;
+  });
+  it("returns false for 'fantasy'", () => {
+    expect(isPalindrome("fantasy")).to.be.false;
   });
   it("returns false for 'ab'", () => {
     expect(isPalindrome("ab")).to.be.false;
